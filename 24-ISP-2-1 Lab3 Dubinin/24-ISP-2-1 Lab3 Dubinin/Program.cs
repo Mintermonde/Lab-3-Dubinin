9594eb507f3b1903cb8e79a1e07b386c446b5363
Mintermonde <shmidt.foton@bk.ru>
2025-10-05 17:14:02 +0200

    first

---
+++ 24-ISP-2-1 Lab3 Dubinin/24-ISP-2-1 Lab3 Dubinin/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = (num / 100) * 100 + (num % 10) * 10 + (num / 10) % 10;
+
+Console.WriteLine($"Результат: {result}");
